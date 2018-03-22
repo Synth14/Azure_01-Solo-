37a94bfbb35c69247f148c7efca47f0367d6e994
Syn Scorps <iasyasdnn@live.fr>
2018-03-22 14:40:09 +0100

    Final

--- Azure_01/Program.cs
+++ Azure_01/Program.cs
@@ -7,20 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Azure_01
+namespace Azure01_Blob
 {
     class Program// ajouter des nuget storage azure
     {
         static void Main(string[] args)
         {
-            CloudStorageAccount storageAcount = new CloudStorageAccount // on veut acceder a azure
-           (new StorageCredentials("Gtm", "clé"),// la on a besoin des codes pour acceder on va les mettre entre (information qu'on recupere sur le compte azure)
-            true);// on met ces deux parametres 
-            CloudBlobClient blobClient = storageAcount.CreateCloudBlobClient(); //creer un compte client 
-            CloudBlobContainer container = blobClient.GetContainerReference("image");
+            try
+            {
+                CloudStorageAccount storageAcount = new CloudStorageAccount // on veut acceder a azure
+               (new StorageCredentials("Gtm", "clé"),// la on a besoin des codes pour acceder on va les mettre entre (information qu'on recupere sur le compte azure)
+                true);// on met ces deux parametres 
+                CloudBlobClient blobClient = storageAcount.CreateCloudBlobClient(); //creer un compte client 
+                CloudBlobContainer container = blobClient.GetContainerReference("image");
 
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference("blob1");
-            using (var filestream = System.IO.File.OpenRead("s:\ali.jpg"));// des qu'on sort du system ,jessy detruit ce fichier la ,pour liberer de la place
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference("blob1");
+                using (var filestream = System.IO.File.OpenRead("s:\ali.jpg"))  // des qu'on sort du system ,jessy detruit ce fichier la ,pour liberer de la place
+                {
+                    blockBlob.UploadFromStreamAsync(filestream);
                 }
+                Console.WriteLine("Le fichier a été chargé sur le serveur");
+            }
+            catch (Exception e) { Console.WriteLine("Erreur: " + e.Message); }
+            Console.Read();
+        }
     }
 }
